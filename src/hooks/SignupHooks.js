6cc009e9d247a286dcc9a2590134076ba13aac99
Brandon Johnson <brandon121j@gmail.com>
2022-01-24 20:59:47 -0500

    Testing git commit

---
+++ src/hooks/SignupHooks.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from React;
+
+function SignupHooks() {
+    const [firstError, setFirstError] = useState('');
+    const [lastError, setLastError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [firstError, setFirstError] = useState('');
+    const changes = 0;
+}
