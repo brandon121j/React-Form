6c4ec7e2ddb4d1506042f65c92b5f7ece139dd99
Brandon Johnson <brandon121j@gmail.com>
2022-01-25 22:13:43 -0500

    Finalized useState hooks in Signup.js

--- src/hooks/SignupHooks.js
+++ src/hooks/SignupHooks.js
@@ -1,9 +1,27 @@
 import { useState, useEffect } from React;
 
 function SignupHooks() {
-    const [firstError, setFirstError] = useState('');
-    const [lastError, setLastError] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [firstError, setFirstError] = useState('');
-    const changes = 0;
+    const [firstErr, setFirstErr] = useState('');
+    const [lastErr, setLastErr] = useState('');
+    const [emailErr, setEmailErr] = useState('');
+    const [userErr, setUserErr] = useState('');
+    const [passErr, setPassErr] = useState('');
+
+    const [firstBlur, setFirstBlur] = useState(false);
+    const [lastBlur, setLastBlur] = useState(false);
+    const [emailBlur, setEmailBlur] = useState(false);
+    const [userBlur, setUserBlur] = useState(false);
+    const [passBlur, setpassBlur] = useState(false);
+
+    const [first, setFirst] = useState('');
+    const [last, setLast] = useState('');
+    const [email, setEmail] = useState('');
+    const [user, setUser] = useState('');
+    const [pass, setPass] = useState('');
+    
+    const [firstErr, setFirstErr] = useState('');
+    const [lastErr, setLastErr] = useState('');
+    const [emailErr, setEmailErr] = useState('');
+    const [userErr, setUserErr] = useState('');
+    const [passErr, setPassErr] = useState('');
 }
